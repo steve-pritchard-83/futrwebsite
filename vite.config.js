3ec0444869a20eb4a5d5323ef124f8dc569e7b8e
steve-pritchard-83 <stevep199@gmail.com>
2025-06-18 05:39:09 +0930

    fix: Simplify vite config for reliable builds

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,11 @@ import react from '@vitejs/plugin-react';
 import commonjs from 'vite-plugin-commonjs';
 
 export default defineConfig({
+  plugins: [react()],
+  root: '.',
   build: {
     outDir: 'build',
   },
-  plugins: [
-    react(),
-    commonjs(),
-  ],
   esbuild: {
     loader: 'jsx',
     include: /src\/.*\.jsx?$/,
